a4c7704bc8aef67900a3f346f05c1ba8d2316a96
Matěj Pavlíček <tikinang@gmail.com>
2024-07-25 18:03:39 +0200

    init

---
+++ src/test/java/io/zerops/recipespring/RecipeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zerops.recipespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
