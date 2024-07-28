085095d6c5f5b476719f8c5f69575dbcabd821ac
Matěj Pavlíček <tikinang@gmail.com>
2024-07-28 13:39:13 +0200

    send emails, progress on .ymls

--- src/main/java/io/zerops/recipespring/FileController.java
+++ src/main/java/io/zerops/recipespring/FileController.java
@@ -1,6 +1,7 @@
 package io.zerops.recipespring;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +18,18 @@ public class FileController {
     private final FileRepository fileRepository;
     private final AmazonS3 s3Client;
     private final S3Config s3Config;
+    private final EmailService emailService;
 
     public FileController(
             FileRepository fileRepository,
             AmazonS3 s3Client,
-            S3Config s3Config
+            S3Config s3Config,
+            EmailService emailService
     ) {
         this.fileRepository = fileRepository;
         this.s3Client = s3Client;
         this.s3Config = s3Config;
+        this.emailService = emailService;
     }
 
     @PostMapping("/file")
@@ -44,8 +48,23 @@ public class FileController {
         fileEntity.path = this.s3Config.createUrl(key);
         this.fileRepository.save(fileEntity);
 
-        var response = this.s3Client.putObject(this.s3Config.bucketName, key, file.getInputStream(), null);
+        var objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(file.getContentType());
+        objectMetadata.setContentLength(file.getSize());
+        var response = this.s3Client.putObject(
+                this.s3Config.bucketName,
+                key,
+                file.getInputStream(),
+                objectMetadata
+        );
+
         this.logger.info(response.getMetadata().getETag());
+
+        this.emailService.sendEmail(
+                "user@zerops.io",
+                "File successfully uploaded.",
+                String.format("File %s (%d bytes) uploaded at: %s", fileEntity.name, fileEntity.size, fileEntity.path)
+        );
 
         return fileEntity;
     }
