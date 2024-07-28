49d3c40bc095658b2ce274490628bb43905c1e51
Matěj Pavlíček <tikinang@gmail.com>
2024-07-28 14:07:05 +0200

    cors?

--- src/main/java/io/zerops/recipespring/FileController.java
+++ src/main/java/io/zerops/recipespring/FileController.java
@@ -4,14 +4,12 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 public class FileController {
     private final Logger logger = LoggerFactory.getLogger(FileController.class);
